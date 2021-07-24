4d57388258f71c2678044b6945800b0920836278
devGlo <ashawani096@gmail.com>
2021-07-24 18:47:37 +0530

    DNBI-7645: dateToStringConvertor added in main

--- PNB_Credit.java
+++ PNB_Credit.java
@@ -1,7 +1,25 @@
+package com.pnb.credit;
+
+import java.text.DateFormat;  
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+import java.util.Calendar;  
+
 class PNB_Credit{
 	
 	public static void main(String... args){
+		System.out.println("Inside Main");
+		Date date = Calendar.getInstance().getTime(); 
+		dateToStringConv(date);
 
-		System.out.println("Main started");
 
+}
+
+private static String dateToStringConv(Date date){
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
+                String strDate = dateFormat.format(date);  
+                return strDate; 
+        
+   }  
+	
 }
